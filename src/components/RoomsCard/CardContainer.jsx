7c2a710e136b90a695eae39623d1226d26d85bd8
detectiveseo <detectiveseo1@gmail.com>
2023-07-10 20:51:45 +0600

    dinamic 404 items

--- src/components/RoomsCard/CardContainer.jsx
+++ src/components/RoomsCard/CardContainer.jsx
@@ -4,6 +4,7 @@ import { useEffect, CSSProperties, useState } from 'react';
 import Card from './Card';
 import DotLoading from '../Shared/Loader/DotLoading';
 import { useSearchParams } from 'react-router-dom';
+import NotFound from '../Shared/Headings/NotFound';
 
 
 const CardContainer = () => {
@@ -14,26 +15,34 @@ const CardContainer = () => {
 
     useEffect(() => {
         axios.get('./Rooms.json').then(res => {
-            if(searchCategory){
-               const rooms = res.data.filter(room => room.category === searchCategory)
-               console.log(rooms)
-               setRooms(rooms);
-            }else{
+            if (searchCategory) {
+                const rooms = res.data.filter(room => room.category === searchCategory)
+                console.log(rooms)
+                setRooms(rooms);
+            } else {
                 setRooms(res.data);
             }
             setLoading(false)
         })
     }, [searchCategory])
 
-    if(loading){
+    if (loading) {
         return <DotLoading />
     }
     return (
-        <div className='grid lg:grid-cols-5 md:grid-cols-4 grid-cols-2 gap-10'>
-            {
-                rooms.map((room, index) => <Card key={index} room={room} />)
-            }
-        </div>
+        <>
+            {rooms && rooms.length > 0 ? <div className='grid lg:grid-cols-5 md:grid-cols-4 grid-cols-2 gap-10'>
+                {
+                    rooms.map((room, index) => <Card key={index} room={room} />)
+                }
+            </div> : <div className='h-[40vh] flex justify-center items-center'>
+                <NotFound
+                    title="Sorry No Avilabe Location"
+                    subtitle="Please Select Other Categoris"
+                    center={true}
+                />
+            </div>}
+        </>
     )
 };
 export default CardContainer;
--- src/components/Shared/Footer/Footer.jsx
+++ src/components/Shared/Footer/Footer.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div>
-            this is the footer 
-        </div>
+        <footer className='px-4 divide-y  text-gray-800 relative bottom-0 left-0'>
+            <div className='py-6 text-sm text-center text-gray-400'>
+                © 2023 AirCNC Inc. All rights reserved.
+            </div>
+        </footer>
     );
 };
 
