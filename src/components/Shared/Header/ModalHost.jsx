3130a27659ae210c217d5a853585fed49da028c3
detectiveseo <detectiveseo1@gmail.com>
2023-07-18 20:21:22 +0600

    put method added for update user

--- src/components/Shared/Header/ModalHost.jsx
+++ src/components/Shared/Header/ModalHost.jsx
@@ -1,23 +1,26 @@
-import React, { useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { Dialog } from '@headlessui/react'
+import updateUser from '../../../API/updateUser';
+import { AuthContext } from '../../../providers/AuthProvider';
 
 const ModalHost = ({setIsModal, isModal}) => {
+    const {user} = useContext(AuthContext);
+    const role = "Host";
+    const email = user?.email;
     function handleDeactivate() {
+        updateUser(email, role);
         setIsModal(false)
     }
     return (
         <Dialog
-            className="text-center w-full bg-black shadow-xl absolute top-0 bottom-0 left-0 right-0 bg-opacity-70 flex justify-center items-center z-[5000]"
+            className="text-center w-full bg-black shadow-xl fixed top-0 bottom-0 left-0 right-0 bg-opacity-70 flex justify-center items-center z-[5000]"
             open={isModal} onClose={() => setIsModal(false)}>
             <Dialog.Panel
             className="bg-white p-10 flex flex-col gap-4 w-4/12 mx-auto"
             >
                 <Dialog.Title
-                className="text-3xl uppercase"
-                >Will be a host?</Dialog.Title>
-                <Dialog.Description>
-                    This will permanently deactivate your account
-                </Dialog.Description>
+                className="text-3xl uppercase font-bold"
+                >Will you be a host?</Dialog.Title>
 
                 <p>
                     Are you sure you want to deactivate your account? All of your data
@@ -28,13 +31,13 @@ const ModalHost = ({setIsModal, isModal}) => {
             You can render additional buttons to dismiss your dialog by setting
             `isOpen` to `false`.
           */}
-                <div className='flex justify-center items-center gap-10'>
+                <div className='flex justify-center items-center gap-5'>
                     <button
-                        className='btn bg-red-700 text-white p-2'
+                        className='btn bg-red-700 w-40 text-white p-2'
                         onClick={() => setIsModal(false)}>Cancel</button>
                     <button
-                        className='btn bg-green-700 text-white p-2'
-                        onClick={handleDeactivate}>Deactivate</button>
+                        className='btn bg-green-700 w-40 text-white p-2'
+                        onClick={handleDeactivate}>Host</button>
                 </div>
             </Dialog.Panel>
         </Dialog>
