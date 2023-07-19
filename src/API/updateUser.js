b21c20e37966116134a8c05bd75dcf3db90db784
detectiveseo <detectiveseo1@gmail.com>
2023-07-19 11:34:00 +0600

    put method fixed

--- src/API/updateUser.js
+++ src/API/updateUser.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 const updateuser = (email, role) => {
     const url = `${import.meta.env.VITE_SERVER_LINK}user-update/?email=${email}`
-    axios.put(url, { role }).then(res => console.log(res.data))
+    axios.put(url, {role} ).then(res => console.log(res.data))
 }
 
 export default updateuser;
